3a527ff0366a26c119fe6f199460759ae1d40f01
James Robson <jrobson@turnitin.com>
2016-07-31 18:44:50 +0100

    Text extraction working on one image but with accuracy issues

--- main.go
+++ main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
 	"os/exec"
 
-	_ "github.com/otiai10/gosseract"
+	gosseract "github.com/otiai10/gosseract"
 	_ "github.com/otiai10/mint"
 )
 
 func main() {
-	splitVideoToFrames()
+	err := splitVideoToFrames()
+	if err == nil {
+		extractText()
+	}
 }
 
-func splitVideoToFrames() {
-	ffmpegCmd := exec.Command("ffmpeg", "-i", "./examples/test.mp4", "-f", "image2", "-pix_fmt", "bgr24", "/output/test%06d.bmp")
-	if ffmpegCmd.Run() != nil {
-		panic("Error running ffmpeg command")
+func extractText() {
+	client, err1 := gosseract.NewClient()
+	if err1 != nil {
+		log.Println(err1)
 	}
+	extractedText, err2 := client.Src("/output/test000003.bmp").Out()
+	if err2 != nil {
+		log.Println(err2)
+	}
+	ioutil.WriteFile("/output/extracted_text.txt", []byte(extractedText), 0644)
+}
+
+func splitVideoToFrames() error {
+	ffmpegCmd := exec.Command("ffmpeg", "-i", "./examples/test.mp4", "-f", "image2", "-pix_fmt", "bgr24", "/output/test%06d.bmp")
+	err := ffmpegCmd.Run()
+	return err
 }
